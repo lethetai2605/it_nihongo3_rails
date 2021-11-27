ea3fcd2440073b287fcc5837c3e976dbcf2d7de0
tai <thetai2605@gmail.com>
2021-11-27 05:14:20 +0000

    Add login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   config.hosts.clear
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books
+  root to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
