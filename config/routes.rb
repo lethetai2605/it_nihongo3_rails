d98273ee07166e07233a759833fc987b514f78ac
tai <thetai2605@gmail.com>
2021-11-27 07:44:43 +0000

    Add Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   devise_for :users
   resources :books
-  root to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
